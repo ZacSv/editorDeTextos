170f85ff133d51daaed23cbb520d168ab322bd83
Isac Vieira <isacvieira2009@hotmail.com>
2023-11-25 10:23:53 -0300

    criando funcionalidade de apagar arquivos

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -13,7 +13,7 @@ namespace TextEditor
         static void Menu()
         {
             Console.Clear();
-            Console.WriteLine("O que deseja fazer: ");
+            Console.WriteLine(" 'O que deseja fazer: ");
             Console.WriteLine("1 - Criar um arquivo");
             Console.WriteLine("2 - Abrir e editar um arquivo");
             Console.WriteLine("3 - Deletar um arquivo");
@@ -30,7 +30,7 @@ namespace TextEditor
 
             }
         }
-        static void Criar() 
+        static void Criar()
         {
             Console.Clear();
             Console.WriteLine("Digite seu texto abaixo (ESC para SAIR) ");
@@ -49,37 +49,78 @@ namespace TextEditor
             }
 
             Salvar(userText);
+            Console.WriteLine("Press any key to return menu");
+            Console.ReadKey();
+            Menu();
         }
 
         static void Abrir() {
 
+            Console.Clear();
             var path = "c:\\dev\\EditorTexto\\ArquivosDeTexto\\";
+            ListarArquivosDoDiretorio(path);
+            var extensaoArquivo = ".txt";
 
-            try
+            Console.WriteLine("Qual o nome do arquivo que deseja abrir: ");
+            var arquivoParaAbrir = Console.ReadLine();
+            var caminhoCompletoArquivo = path + arquivoParaAbrir + extensaoArquivo;
+            using (var abrirArquivo = new StreamReader(caminhoCompletoArquivo))
             {
-                if (Directory.Exists(path))
-                {
-                    string[] arquivos = Directory.GetFiles(path);
-                    Console.WriteLine("Arquivos no diretório: ");
-                    foreach (string arquivo in arquivos)
-                    {
-                        Console.WriteLine(arquivo);
-                    }
-                    Console.WriteLine("Cheguei aqui");
-                }
-                else
-                {
-                    Console.WriteLine("O diretório não existe.");
-                }
+                string textoDoArquivo = abrirArquivo.ReadToEnd();
+                Console.WriteLine(textoDoArquivo);
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Ocorreu um erro: {ex.Message}");
-            }
+            Console.WriteLine("Press any key to return menu");
+            Console.ReadKey();
+            Console.Clear();
+            Menu();
+
         }
 
-        static void Apagar() { }
+        static void Apagar() {
+            try
+            {
+                do
+                {
+                    //Lógica para definir nome e caminho do arquivo
+                    var path = "c:\\dev\\EditorTexto\\ArquivosDeTexto\\";
+                    var extensaoArquivo = ".txt";
+                    Console.Clear();
+                    ListarArquivosDoDiretorio(path);
+                    Console.WriteLine("Digite o nome do arquivo que deseja apagar: ");
+                    var nomeArquivo = Console.ReadLine();
+                    var arquivoQueSeraApagado = path + nomeArquivo + extensaoArquivo;
+                    if (File.Exists(arquivoQueSeraApagado))
+                    {
+                        Console.WriteLine("Tem certeza que deseja excluir o arquivo ? (Pressione S para SIM e qualquer outra tecla para NÃO");
+                        string confirmacaoExclusao = Console.ReadLine();
 
+                        //Garante a confirmação de exclusão
+                        if (confirmacaoExclusao.ToUpper() == "S")
+                        {
+                            File.Delete(arquivoQueSeraApagado);
+                            File.Delete(arquivoQueSeraApagado);
+                            Console.WriteLine("Arquivo excluído com sucesso !");
+                            Console.WriteLine("Press any key return to menu");
+                            Console.ReadKey();
+                        }
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine($"Operação abortada, {nomeArquivo + extensaoArquivo} inexistente no diretório !\nVerifique o nome do arquivo e tente novamente");
+                    }
+                    Console.WriteLine("Pressione qualquer tecla para prosseguir e ESC para sair\n");
+                }
+                while (Console.ReadKey().Key != ConsoleKey.Escape);//Enquanto essa condição não for satisfeita  
+                    {
+                    }
+            }
+            finally
+            {
+                Menu();
+            }
+        }
+            
         static void Salvar(string text)
         {
             Console.Clear();
@@ -95,5 +136,33 @@ namespace TextEditor
 
             Console.WriteLine($"Arquivo {nomeDoArquivo} salvo com sucesso !");
         }
-    }  
+
+        static void ListarArquivosDoDiretorio(string caminhoComNomeDoArquivo)
+        {
+            try
+            {
+                if (Directory.Exists(caminhoComNomeDoArquivo))
+                {
+                    string[] arquivos = Directory.GetFiles(caminhoComNomeDoArquivo);
+                    
+                    Console.WriteLine("ARQUIVOS NO DIRETÓRIO");
+                    Console.WriteLine("-------------------------------------------");
+                    foreach (string arquivo in arquivos)
+                    {
+                        Console.WriteLine(arquivo);
+                    }
+                    Console.WriteLine("-------------------------------------------");
+                }
+                else
+                {
+                    Console.WriteLine("O diretório não existe.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+            }
+        }
+
+    }    
 }
--- TextEditor/obj/Debug/net7.0/TextEditor.AssemblyInfo.cs
+++ TextEditor/obj/Debug/net7.0/TextEditor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3235af7bac940990bf35b4e9c2128d0ea295a921")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e8a86fb32c5c3845d88e48700ce8027a63285fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
